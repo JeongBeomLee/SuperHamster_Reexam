dd3509dd014c6290bcfc7442e4d7907c5d3b37e5
Teterschaft <gofla2242@tukorea.ac.kr>
2024-09-11 10:08:53 +0900

    Update SceneManager.cpp

--- Client/Engine/SceneManager.cpp
+++ Client/Engine/SceneManager.cpp
@@ -356,7 +356,6 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 				gameObject->GetTransform()->SetLocalRotation(Vec3(-XM_PIDIV2, 0.f, 0.f));
 				gameObject->GetTransform()->SetLocalScale(Vec3(250.f, 250.f, 250.f));
 				scene->AddGameObject(gameObject);
-				//gameObject->AddComponent(make_shared<TestAnimation>());
 				gameObject->AddComponent(make_shared<PlayerMove>(playerID));
 
 				GET_SINGLE(PlayerManager)->CreatePlayer(playerID, true, gameObject);
@@ -364,24 +363,24 @@ shared_ptr<Scene> SceneManager::LoadTestScene()
 		}
 
 		{
-			shared_ptr<MeshData> meshData = GET_SINGLE(Resources)->LoadFBX(L"..\\Resources\\FBX\\Hamster.fbx");
+			//shared_ptr<MeshData> meshData = GET_SINGLE(Resources)->LoadFBX(L"..\\Resources\\FBX\\Hamster.fbx");
 
-			vector<shared_ptr<GameObject>> gameObjects = meshData->Instantiate();
-			int playerID = GEngine->GetOtherPlayerId();
-			for (auto& gameObject : gameObjects)
-			{
-				gameObject->SetName(L"Player" + to_wstring(playerID));
-				gameObject->SetCheckFrustum(false);
-				gameObject->SetStatic(false);
-				gameObject->GetTransform()->SetLocalPosition(Vec3(-260.224, 0, 60.2587));
-				gameObject->GetTransform()->SetLocalRotation(Vec3(-XM_PIDIV2, 0.f, 0.f));
-				gameObject->GetTransform()->SetLocalScale(Vec3(250.f, 250.f, 250.f));
-				scene->AddGameObject(gameObject);
-				//gameObject->AddComponent(make_shared<TestAnimation>());
-				gameObject->AddComponent(make_shared<PlayerMove>(playerID));
+			//vector<shared_ptr<GameObject>> gameObjects = meshData->Instantiate();
+			//int playerID = GEngine->GetOtherPlayerId();
+			//for (auto& gameObject : gameObjects)
+			//{
+			//	gameObject->SetName(L"Player" + to_wstring(playerID));
+			//	gameObject->SetCheckFrustum(false);
+			//	gameObject->SetStatic(false);
+			//	gameObject->GetTransform()->SetLocalPosition(Vec3(-260.224, 0, 60.2587));
+			//	gameObject->GetTransform()->SetLocalRotation(Vec3(-XM_PIDIV2, 0.f, 0.f));
+			//	gameObject->GetTransform()->SetLocalScale(Vec3(250.f, 250.f, 250.f));
+			//	scene->AddGameObject(gameObject);
+			//	//gameObject->AddComponent(make_shared<TestAnimation>());
+			//	gameObject->AddComponent(make_shared<PlayerMove>(playerID));
 
-				GET_SINGLE(PlayerManager)->CreatePlayer(playerID, false, gameObject);
-			}
+			//	GET_SINGLE(PlayerManager)->CreatePlayer(playerID, false, gameObject);
+			//}
 		}
 
 		//{
